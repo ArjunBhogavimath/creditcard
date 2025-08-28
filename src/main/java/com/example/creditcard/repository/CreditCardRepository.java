22c50a83c756bbee4cc50e067760f35367a11f14
Mallikarjunaiah B M <vpmallikarjuna03@gmail.com>
2025-08-28 21:41:38 +0530

    Added Credit card repository

--- src/main/java/com/example/creditcard/repository/CreditCardRepository.java
+++ src/main/java/com/example/creditcard/repository/CreditCardRepository.java
@@ -1,7 +1,9 @@
 package com.example.creditcard.repository;
 
+import com.example.creditcard.model.CreditCard;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CreditCardRepository {
+public interface CreditCardRepository extends JpaRepository<CreditCard, Long> {
 }
